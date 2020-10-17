9ea54abecabc25927a7125775cc6021d87890a16
kirillsidorov <sidorov.kirill198423@gmail.com>
2020-10-17 14:45:18 +0300

    connection heroku db

--- includes/connection.php
+++ includes/connection.php
@@ -1,15 +1,17 @@
 <?php 
-        $host = '';
-        $dbname = '';
-        $username = '';
-        $password = '';
-        
-        if ($_SERVER['SERVER_NAME'] == "thawing-island-242342379.herokuapp.com") {
-            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        if ($_SERVER['SERVER_NAME'] == "todowebappforgarage.herokuapp.com") {
+            /*$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
             $host = $url["host"];
             $username = $url["user"];
             $password = $url["pass"];
             $dbname = substr($url["path"], 1);
+        */
+        $host = 'eu-cdbr-west-03.cleardb.net';
+        $dbname = 'heroku_497ae4fd7fac821';
+        $username = 'ba9a49bb38ec81';
+        $password = '82979d1b';
+
+
         } else {
             $host = 'localhost';
             $username = 'root';
