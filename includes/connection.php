5a1568e4a2451873bd3a71ecda22833a3a3285f1
kirillsidorov <sidorov.kirill198423@gmail.com>
2020-10-17 14:16:52 +0300

    connection for db cleardb in heroku

--- includes/connection.php
+++ includes/connection.php
@@ -1,9 +1,24 @@
 <?php 
-
-        $con = mysqli_connect('localhost','root','root','todoapp');
-        if(!$con)
-        {
-            die (' Please Your Connectino '.mysqli_error());
+        $host = '';
+        $dbname = '';
+        $username = '';
+        $password = '';
+        
+        if ($_SERVER['SERVER_NAME'] == "thawing-island-242342379.herokuapp.com") {
+            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $host = $url["host"];
+            $username = $url["user"];
+            $password = $url["pass"];
+            $dbname = substr($url["path"], 1);
+        } else {
+            $host = 'localhost';
+            $username = 'root';
+            $password = 'root';
+            $dbname = 'todoapp';
         }
 
-?>
+        $con = mysqli_connect($host, $username,$password,$dbname);
+        
+        if(!$con){
+            die (' Please Your Connectino '.mysqli_error());
+        }?>
